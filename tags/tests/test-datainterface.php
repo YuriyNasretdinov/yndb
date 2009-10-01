2ea70e386b4d195445d5a3a8830756b4151e426c
nasretdinov <nasretdinov@04257188-a499-11de-8a4e-6d34bb13bdfc>
2009-10-01 01:44:19 +0000

    Little change to test.php, so that it deletes the entire directory with data, with subdirectories

--- tags/tests/test-datainterface.php
+++ tags/tests/test-datainterface.php
@@ -31,140 +31,140 @@ function print_footer($res)
 
 echo '<h3>All columns:</h3>';
 
-$DI = new YNDataInterface('data');
+		$DI = new YNDataInterface('data');
 
-$i = 0;
+		$i = 0;
 
-$start_time = microtime(true);
+		$start_time = microtime(true);
 
-$dd = $DI->openTable_FullScan('test3') or die($DI->get_error());
+		$dd = $DI->openTable_FullScan('test3') or die($DI->get_error());
 
-while($res = $DI->fetchRow_FullScan($dd))
-{
-	$i++;
+		while($res = $DI->fetchRow_FullScan($dd))
+		{
+			$i++;
 	
-	//if(!$i++) print_header($res);
+			//if(!$i++) print_header($res);
 	
-	//print_res($res);
-}
+			//print_res($res);
+		}
 
-$DI->closeTable_FullScan($dd);
+		$DI->closeTable_FullScan($dd);
 
-echo 'DataInterface took '.round( microtime(true) - $start_time, 4 ).' sec ('.$i.' results)<br>';
+		echo 'DataInterface took '.round( microtime(true) - $start_time, 4 ).' sec ('.$i.' results)<br>';
 
-$start_time = microtime(true);
+		$start_time = microtime(true);
 
-$res = $DI->select( 'test3', array( 'limit' => 10000000 ) );
+		$res = $DI->select( 'test3', array( 'limit' => 10000000 ) );
 
-echo 'select() method took '.round( microtime(true) - $start_time, 4 ).' sec ('.sizeof($res).' results)<br>';
+		echo 'select() method took '.round( microtime(true) - $start_time, 4 ).' sec ('.sizeof($res).' results)<br>';
 
-$random_pri = $res[ rand(0,sizeof($res)-1) ]['id'];
-$random_idx = $res[ rand(0,sizeof($res)-1) ]['bad_rand'];
-$random_uni = $res[ rand(0,sizeof($res)-1) ]['rand'];
+		$random_pri = $res[ rand(0,sizeof($res)-1) ]['id'];
+		$random_idx = $res[ rand(0,sizeof($res)-1) ]['bad_rand'];
+		$random_uni = $res[ rand(0,sizeof($res)-1) ]['rand'];
 
 echo '<h3>Several columns:</h3>';
 
-$i = 0;
+		$i = 0;
 
-$start_time = microtime(true);
+		$start_time = microtime(true);
 
-$dd = $DI->openTable_FullScan('test3', array('rand', 'another_rand')) or die($DI->get_error());
+		$dd = $DI->openTable_FullScan('test3', array('rand', 'another_rand')) or die($DI->get_error());
 
-while($res = $DI->fetchRow_FullScan($dd))
-{
-	$i++;
+		while($res = $DI->fetchRow_FullScan($dd))
+		{
+			$i++;
 	
-	//if(!$i++) print_header($res);
+			//if(!$i++) print_header($res);
 	
-	//print_res($res);
-}
+			//print_res($res);
+		}
 
-$DI->closeTable_FullScan($dd);
+		$DI->closeTable_FullScan($dd);
 
-echo 'DataInterface took '.round( microtime(true) - $start_time, 4 ).' sec ('.$i.' results)<br>';
+		echo 'DataInterface took '.round( microtime(true) - $start_time, 4 ).' sec ('.$i.' results)<br>';
 
-$start_time = microtime(true);
+		$start_time = microtime(true);
 
-$res = $DI->select( 'test3', array( 'col' => 'rand,another_rand', 'limit' => 10000000 ) );
+		$res = $DI->select( 'test3', array( 'col' => 'rand,another_rand', 'limit' => 10000000 ) );
 
-echo 'select() method took '.round( microtime(true) - $start_time, 4 ).' sec ('.sizeof($res).' results)<br>';
+		echo 'select() method took '.round( microtime(true) - $start_time, 4 ).' sec ('.sizeof($res).' results)<br>';
 
-//print_footer($res);
+		//print_footer($res);
 
 echo '<h3>Using index:</h3>';
 
-echo 'Fetching:<br><br>';
+		echo 'Fetching:<br><br>';
 
-echo 'id: '.$random_pri.'<br>';
-echo 'bad_rand: '.$random_idx.'<br>';
-echo 'rand: '.$random_uni.'<br>';
+		echo 'id: '.$random_pri.'<br>';
+		echo 'bad_rand: '.$random_idx.'<br>';
+		echo 'rand: '.$random_uni.'<br>';
 
-echo '<h4>id (PRIMARY)</h4>';
+		echo '<h4>id (PRIMARY)</h4>';
 
-$start_time = microtime(true);
+			$start_time = microtime(true);
 
-$i = 0;
+			$i = 0;
 
-$dd = $DI->openTable_Index_ExactMatch('test3', array('rand', 'another_rand'), 'id', $random_pri) or die($DI->get_error());
+			$dd = $DI->openTable_Index_ExactMatch('test3', array('rand', 'another_rand'), 'id', $random_pri) or die($DI->get_error());
 
-while($res = $DI->fetchRow_Index_ExactMatch($dd))
-{
-	$i++;
+			while($res = $DI->fetchRow_Index_ExactMatch($dd))
+			{
+				$i++;
 	
-	//if(!$i++) print_header($res);
+				//if(!$i++) print_header($res);
 	
-	//print_res($res);
-}
+				//print_res($res);
+			}
 
-$DI->closeTable_Index_ExactMatch($dd);
+			$DI->closeTable_Index_ExactMatch($dd);
 
-echo 'DataInterface took '.round( microtime(true) - $start_time, 4 ).' sec ('.$i.' results)<br>';
+			echo 'DataInterface took '.round( microtime(true) - $start_time, 4 ).' sec ('.$i.' results)<br>';
 
-if($i != 1) die('Test failed for ID field');
+			if($i != 1) die('Test failed for ID field');
 
-echo '<h4>bad_rand (INDEX)</h4>';
+		echo '<h4>bad_rand (INDEX)</h4>';
 
-$start_time = microtime(true);
+			$start_time = microtime(true);
 
-$i = 0;
+			$i = 0;
 
-$dd = $DI->openTable_Index_ExactMatch('test3', array('rand', 'another_rand'), 'bad_rand', $random_idx) or die($DI->get_error());
+			$dd = $DI->openTable_Index_ExactMatch('test3', array('rand', 'another_rand'), 'bad_rand', $random_idx) or die($DI->get_error());
 
-while($res = $DI->fetchRow_Index_ExactMatch($dd))
-{
-	$i++;
+			while($res = $DI->fetchRow_Index_ExactMatch($dd))
+			{
+				$i++;
 	
-	//if(!$i++) print_header($res);
+				//if(!$i++) print_header($res);
 	
-	//print_res($res);
-}
+				//print_res($res);
+			}
 
-$DI->closeTable_Index_ExactMatch($dd);
+			$DI->closeTable_Index_ExactMatch($dd);
 
-echo 'DataInterface took '.round( microtime(true) - $start_time, 4 ).' sec ('.$i.' results)<br>';
+			echo 'DataInterface took '.round( microtime(true) - $start_time, 4 ).' sec ('.$i.' results)<br>';
 
-if($i != sizeof( $DI->select('test3', array('cond' => 'bad_rand = '.$random_idx)) )) die('Test failed for BAD_RAND field');
+			if($i != sizeof( $DI->select('test3', array('cond' => 'bad_rand = '.$random_idx)) )) die('Test failed for BAD_RAND field');
 
-echo '<h4>rand (UNIQUE)</h4>';
+		echo '<h4>rand (UNIQUE)</h4>';
 
-$start_time = microtime(true);
+			$start_time = microtime(true);
 
-$i = 0;
+			$i = 0;
 
-$dd = $DI->openTable_Index_ExactMatch('test3', array('rand', 'another_rand'), 'rand', $random_uni) or die($DI->get_error());
+			$dd = $DI->openTable_Index_ExactMatch('test3', array('rand', 'another_rand'), 'rand', $random_uni) or die($DI->get_error());
 
-while($res = $DI->fetchRow_Index_ExactMatch($dd))
-{
-	$i++;
+			while($res = $DI->fetchRow_Index_ExactMatch($dd))
+			{
+				$i++;
 	
-	//if(!$i++) print_header($res);
+				//if(!$i++) print_header($res);
 	
-	//print_res($res);
-}
+				//print_res($res);
+			}
 
-$DI->closeTable_Index_ExactMatch($dd);
+			$DI->closeTable_Index_ExactMatch($dd);
 
-echo 'DataInterface took '.round( microtime(true) - $start_time, 4 ).' sec ('.$i.' results)<br>';
+			echo 'DataInterface took '.round( microtime(true) - $start_time, 4 ).' sec ('.$i.' results)<br>';
 
-if($i != 1) die('Test failed for RAND field');
+			if($i != 1) die('Test failed for RAND field');
 ?>
